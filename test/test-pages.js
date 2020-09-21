d20a07460420df9407885e61e6d70b9be31584d9
Lucaslah <47047328+Lucaslah@users.noreply.github.com>
2020-09-21 13:09:04 +1200

    Bulk Update and add Files to Github

--- test/test-pages.js
+++ test/test-pages.js
@@ -1,16 +1,16 @@
-var expect  = require('chai').expect;
+var expect = require('chai').expect;
 var request = require('request');
 
 it('InkStatus server status', function(done) {
-    request('http://localhost:3000' , function(error, response, body) {
+    request('http://localhost:5000', function(error, response, body) {
         expect(response.statusCode).to.equal(200);
         done();
     });
 });
 
-it('PHP Web Server', function(done) {
-    request('http://localhost:3001' , function(error, response, body) {
-        expect(response.statusCode).to.equal(200);
+it('InkStatus PHP server status', function(done) {
+    request('http://localhost:5000/php', function(error, response, body) {
+        expect(response.statusCode).to.equal(404);
         done();
     });
 });
---
+++ tests.php
@@ -0,0 +1,3 @@
+<?php
+echo("The Server is Working");
+?>
