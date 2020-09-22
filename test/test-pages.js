fd0f7674d0e11db9e2cd64e9983294f849813bc9
Lucas <47047328+Lucaslah@users.noreply.github.com>
2020-09-22 12:33:50 +1200

    Update test-pages.js

--- test/test-pages.js
+++ test/test-pages.js
@@ -3,14 +3,8 @@ var request = require('request');
 
 it('InkStatus server status', function(done) {
     request('http://localhost:5000', function(error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        done();
-    });
-});
-
-it('InkStatus PHP server status', function(done) {
-    request('http://localhost:5000/php', function(error, response, body) {
         expect(response.statusCode).to.equal(404);
         done();
     });
 });
+
